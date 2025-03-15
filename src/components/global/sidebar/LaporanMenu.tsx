64277e50436072666b1f0951d52847258feacab2
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-03-15 16:35:27 +0700

    finalize menu laporan

--- src/components/global/sidebar/LaporanMenu.tsx
+++ src/components/global/sidebar/LaporanMenu.tsx
@@ -5,30 +5,32 @@ import { usePathname } from "next/navigation";
 
 const PathNameRegex = /^\/laporan\/.*/;
 
-function useIsOpened() {
-  const currentPath = usePathname();
-
-  return PathNameRegex.test(currentPath) ? "bg-white text-gray-800" : "hover:bg-slate-500";
+function useIsOpened(currentPath: string): boolean {
+  return PathNameRegex.test(currentPath);
 }
 
 export default function LaporanMenu() {
+  const currentPath = usePathname();
+  const isOpened = useIsOpened(currentPath);
 
-  const [Laporan, setLaporan] = useState<boolean | null>(false);
-  const isOpened = useIsOpened();
+  const [laporanOpen, setLaporanOpen] = useState<boolean | null>(false);
 
   useEffect(() => {
-    setLaporan(isOpened === "bg-white text-gray-800");
-  }, [isOpened]);
+    setLaporanOpen(isOpened);
+  }, [isOpened, currentPath]);
 
   return (
     <>
-      <li onClick={() => setLaporan(!Laporan)}
+      <li onClick={() => setLaporanOpen((prev) => !prev)}
         className="flex font-medium items-center gap-x-2 cursor-pointer p-2 hover:bg-slate-500 rounded-xl transition-all duration-300 ease-in-out" >
         <TbBook className="text-xl" />
         <span className={`origin-left duration-200`}>Laporan</span>
       </li>
       {/* SUB MENU LAPORAN */}
-      <div className={`transition-all duration-300 ease-in-out ${Laporan ? 'px-3 py-2 flex flex-col border-l-2 border-white rounded-b-xl ml-2  max-h-screen opacity-100' : 'max-h-0 opacity-0 pointer-events-none'}`}>
+      <div className={`transition-all duration-300 ease-in-out 
+        ${laporanOpen
+          ? 'px-3 py-2 flex flex-col border-l-2 border-white rounded-b-xl ml-2  max-h-screen opacity-100'
+          : 'max-h-0 opacity-0 pointer-events-none'}`}>
         {/* LABEL LAPORAN USULAN */}
         <SupportingPokin />
       </div >
