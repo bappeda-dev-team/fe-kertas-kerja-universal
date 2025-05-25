5c7d9381d4d837353640eff925646aea1b6a1991
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-05-26 03:11:10 +0700

    add client user context for handle nama client

--- src/app/DataMaster/masteropd/page.tsx
+++ src/app/DataMaster/masteropd/page.tsx
@@ -4,36 +4,36 @@ import { FiHome } from "react-icons/fi";
 import { TbCirclePlus } from "react-icons/tb";
 import { ButtonSky } from "@/components/global/Button";
 import { TableMasterOpd } from "@/components/pages/datamaster/masteropd";
-
+import { useAppContext } from "@/context/AppContext"
 
 function masteropd() {
-    return (
-        <>
-            <div className="flex items-center">
-                <a href="/" className="mr-1"><FiHome /></a>
-                <p className="mr-1">/ Data Master</p>
-                <p className="mr-1">/ Master OPD</p>
-            </div>
-            <div className="mt-3 rounded-xl shadow-lg border">
-                <div className="flex items-center justify-between border-b px-5 py-5">
-                    <div className="flex flex-col items-end">
-                        {/* TODO: MAKE IT DYNAMIC FROM BACKEND, CHOOSE WHAT LEMBAGA AKTIF */}
-                        <h1 className="uppercase font-bold">Daftar OPD Kabupaten Mahakam Ulu</h1>
-                    </div>
-                    <div className="flex flex-col">
-                        <ButtonSky
-                            className="flex items-center justify-center"
-                            halaman_url='/DataMaster/masteropd/tambah'
-                        >
-                            <TbCirclePlus className="mr-1" />
-                            Tambah OPD
-                        </ButtonSky>
-                    </div>
-                </div>
-                <TableMasterOpd />
-            </div>
-        </>
-    )
+  const { clientUser } = useAppContext();
+  return (
+    <>
+      <div className="flex items-center">
+        <a href="/" className="mr-1"><FiHome /></a>
+        <p className="mr-1">/ Data Master</p>
+        <p className="mr-1">/ Master OPD</p>
+      </div>
+      <div className="mt-3 rounded-xl shadow-lg border">
+        <div className="flex items-center justify-between border-b px-5 py-5">
+          <div className="flex flex-col items-end">
+            <h1 className="uppercase font-bold">Daftar OPD {clientUser}</h1>
+          </div>
+          <div className="flex flex-col">
+            <ButtonSky
+              className="flex items-center justify-center"
+              halaman_url='/DataMaster/masteropd/tambah'
+            >
+              <TbCirclePlus className="mr-1" />
+              Tambah OPD
+            </ButtonSky>
+          </div>
+        </div>
+        <TableMasterOpd />
+      </div>
+    </>
+  )
 }
 
 export default masteropd;
--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -5,14 +5,17 @@ import { useState, createContext, useContext } from "react";
 interface AppContextType {
   appName: string;
   setAppName: (name: string) => void;
+  clientUser: string;
 }
 const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "KERTAS-KERJA";
+const CLIENT_USER = process.env.NEXT_PUBLIC_CLIENT_USER || "--"; // menunjuk ke nama lembaga client (klien) asli
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
+// for global static text for branding purposes
 export function AppProvider({ children }: Readonly<{ children: React.ReactNode; }>) {
   const [appName, setAppName] = useState(APP_NAME);
   return (
-    <AppContext.Provider value={{ appName, setAppName }}>
+    <AppContext.Provider value={{ appName, setAppName, clientUser: CLIENT_USER }}>
       {children}
     </AppContext.Provider>
   );
